34d6b58426809adedd7bfbc5ce0d41fdaee0e9c3
Charlie <charlieedoherty@gmail.com>
2024-07-15 16:05:35 -0500

    Make styles and scripts available from the server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-app.use(express.urlencoded({ "extended": false }))
+app.use(express.static("public"))
 
 
 app.get("/about", function (req, res) {
